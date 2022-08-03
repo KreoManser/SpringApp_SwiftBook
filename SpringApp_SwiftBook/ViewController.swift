0db291413be3f31a108fa47edec5de33bcee2c47
KreoManser <sherba.ru@list.ru>
2022-08-04 02:23:03 +0700

    Initial Commit

---
+++ SpringApp_SwiftBook/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SpringApp_SwiftBook
+//
+//  Created by Сергей Бабич on 04.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

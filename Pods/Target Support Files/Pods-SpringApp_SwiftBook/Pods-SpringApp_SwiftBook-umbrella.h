f82191d905be025483042d92ab321d27872be023
KreoManser <sherba.ru@list.ru>
2022-08-04 04:18:25 +0700

    setup and complete project

---
+++ Pods/Target Support Files/Pods-SpringApp_SwiftBook/Pods-SpringApp_SwiftBook-umbrella.h
@@ -0,0 +1,16 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+
+FOUNDATION_EXPORT double Pods_SpringApp_SwiftBookVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_SpringApp_SwiftBookVersionString[];
+
--- SpringApp_SwiftBook/ViewController.swift
+++ SpringApp_SwiftBook/ViewController.swift
@@ -6,14 +6,74 @@
 //
 
 import UIKit
+import Spring
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    // MARK: - Outlets
+    
+    @IBOutlet weak var animationView: SpringView!
+    @IBOutlet weak var presetLabel: UILabel!
+    @IBOutlet weak var curveLabel: UILabel!
+    @IBOutlet weak var forceLabel: UILabel!
+    @IBOutlet weak var durationLabel: UILabel!
+    @IBOutlet weak var delayLabel: UILabel!
+    @IBOutlet weak var animationButton: UIButton!
+    
+    // MARK: - Properties
+    
+    let animations = Spring.AnimationPreset.allCases
+    let curves = Spring.AnimationCurve.allCases
+    
+    var animationCounter = 0
+    var curvesCounter = 0
+    
+    // MARK: - Private methods
+    
+    private func setupLabels(force: CGFloat, duration: CGFloat, delay: CGFloat) {
+        presetLabel.text = "Preset: \(animations[animationCounter])"
+        curveLabel.text = "Curve: \(curves[curvesCounter])"
+        forceLabel.text = "Force: \(String(format: "%.2f", force))"
+        durationLabel.text = "Duration: \(String(format: "%.2f", duration))"
+        delayLabel.text = "Delay: \(String(format: "%.2f", delay))"
+    }
+    
+    private func checkAnimationCounter() {
+        if animationCounter < 26 {
+            animationCounter += 1
+        } else {
+            animationCounter = 0
+        }
+    }
+    
+    private func checkCurveCounter() {
+        if curvesCounter < 28 {
+            curvesCounter += 1
+        } else {
+            curvesCounter = 0
+        }
+    }
+    
+    private func setupAnimation() {
+        let force = CGFloat.random(in: 0...2)
+        let duration = CGFloat.random(in: 0...2)
+        let delay = CGFloat.random(in: 0...1)
+        
+        animationView.animation = "\(animations[animationCounter].rawValue)"
+        animationView.curve = "\(curves[curvesCounter].rawValue)"
+        animationView.force = force
+        animationView.duration = duration
+        animationView.delay = delay
+        animationView.animate()
+        setupLabels(force: force, duration: duration, delay: delay)
     }
 
-
+    // MARK: - Actions
+    
+    @IBAction func animationButtonDidTap(_ sender: UIButton) {
+        setupAnimation()
+        checkAnimationCounter()
+        animationButton.setTitle("Run \(animations[animationCounter])", for: .normal)
+        checkCurveCounter()
+    }
 }
 
